61de7afe9cec6a7785b476212bc7e214e059c6f8
+ <+>
2016-03-03 19:40:07 +0800

    frist

---
+++ China-Act/China-Act/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  China-Act
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 练晓俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
